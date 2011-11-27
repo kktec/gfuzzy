2af60651c32ac319408d88de428be9e93ab0b4d8
Ken Krebs <kk@kktec.com>
2011-11-27 17:53:30 -0600

    added stringify of rules by inferencer

--- gfuzzy-core/src/main/groovy/org/gfuzzy/inference/Inferencer.groovy
+++ gfuzzy-core/src/main/groovy/org/gfuzzy/inference/Inferencer.groovy
@@ -5,7 +5,13 @@ import org.gfuzzy.FuzzySet
 
 class Inferencer {
 	
+	final String name
+	
 	List<Rule> rules = []
+	
+	Inferencer(String name) {
+		this.name = name
+	}
 	
 	void infer(Map<String, FuzzySet> inputs, FuzzySet output) {
 		rules.each { rule ->
@@ -16,6 +22,24 @@ class Inferencer {
 	Inferencer rule(String zone, Map<String, String>predicates) {
 		rules << new Rule(zone, predicates)
 		this
+	}
+	
+	List<String> stringify() {
+		List<String> strings = []
+		rules.each { rule ->
+			def string = 'IF '
+			boolean firstPredicate = true
+			rule.predicates.each { key, value ->
+				if (!firstPredicate) {
+					string += 'AND '
+				}
+				string += "$key.$value "
+				firstPredicate = false
+			}
+			string += "THEN $name.$rule.zone"
+			strings << string
+		}
+		strings
 	}
 	
 	private Fuzzy processRule(Map inputs, rule) {
--- gfuzzy-core/src/test/groovy/org/gfuzzy/inference/InferencerTests.groovy
+++ gfuzzy-core/src/test/groovy/org/gfuzzy/inference/InferencerTests.groovy
@@ -12,7 +12,7 @@ class InferencerTests extends GroovyTestCase {
 	FuzzySetDefinition inputSet2 = definitionForPeaks("input2", [N:-25, Z:0, P:25])
 	FuzzySetDefinition outputSet = definitionForPeaks("output", [N:-2.5, Z:0, P:2.5])
 
-	Inferencer inferencer = new Inferencer()
+	Inferencer inferencer = new Inferencer('output')
 
 	def input0 = inputSet0.fuzzify(2.5)
 	def input1 = inputSet1.fuzzify(5)
@@ -33,6 +33,17 @@ class InferencerTests extends GroovyTestCase {
 				.rule('Z', [input0: 'Z', input1: 'Z', input2: 'Z'])
 				.rule('P', [input0: 'Z', input1: 'P'])
 		assertRules()
+	}
+	
+	void test_stringify() {
+		inferencer.rule('N', [input0: 'Z']).rule('X', [input0: 'P', input1: 'Z', input2: 'N'])
+		def expected = [
+			"IF input0.Z THEN output.N",
+			"IF input0.P AND input1.Z AND input2.N THEN output.X"
+		]
+		def actual = inferencer.stringify()
+		assert 2 == actual.size()
+		assert expected == actual
 	}
 
 	private assertRules() {
